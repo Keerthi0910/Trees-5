709165283c952ab266b18b7047d298771c59b5ad
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-19 23:00:12 -0500

    Add tree recovery method to fix swapped nodes
    Implement tree recovery by swapping nodes during inorder traversal.

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,50 @@
+// s9wapping nodes during inorder traversal as the values in sorted order
+// O(n) time complexity
+// O(1) space complexity
 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev;
+    TreeNode first, second;
+
+    public void recoverTree(TreeNode root) {
+        helper(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void helper(TreeNode root) {
+        if(root == null) return;
+
+        helper(root.left);
+
+        if(prev != null && prev.val >= root.val) {
+            if(first == null) {
+                first = prev;
+                second = root;
+            } else {
+                second = root;
+            }
+        }
+
+        prev = root;
+
+        helper(root.right);
+    }
+}
